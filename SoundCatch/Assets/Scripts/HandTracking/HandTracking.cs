03a3e052b8e71d5c2e53e28857046dc0b9a7255e
Yeon09-a <kimyeona0926@gmail.com>
2024-05-04 19:11:55 +0900

    2024.05.04 게임 선택 화면 수정_Yeon09a
    -게임 선택 화면 씬 수정

--- SoundCatch/Assets/Scripts/HandTracking/HandTracking.cs
+++ SoundCatch/Assets/Scripts/HandTracking/HandTracking.cs
@@ -102,6 +102,9 @@ public class HandTracking : MonoBehaviour
                     }
                     else if (hit.collider.gameObject.layer == LayerMask.NameToLayer("GameObject")) // 인식한 오브젝트가 GameObject인 경우
                     {
+
+                        SceneLoader.Instance.mainGame = MainGame.causeSound;
+
                         // 게임 구분
                         switch (SceneLoader.Instance.mainGame)
                         {
