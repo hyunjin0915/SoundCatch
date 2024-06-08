4d72b4acf7329bb6b1286f0a89bb1f1c90f94f4b
Toothless <hyunjinney@naver.com>
2024-06-09 08:49:42 +0900

    [Fix] 3초 안내음성 수정

--- SoundCatch/Assets/Scripts/VoiceGuide/AudioGuideManager.cs
+++ SoundCatch/Assets/Scripts/VoiceGuide/AudioGuideManager.cs
@@ -12,24 +12,6 @@ public class AudioGuideManager : MonoBehaviour
         audioSource.clip = _audioInfoSO.audioClip;
         audioSource?.Play();
 
-        //if(!audioSource.isPlaying)
-        //{
-        //    StartCoroutine(waitAseconds());
-        //}
-
-        if(SceneLoader.Instance.curSceneName.Equals("HiddenSound"))
-        {
-            StartCoroutine(WaitHiddenSound());
-        }
-        else if(SceneLoader.Instance.curSceneName.Equals("TuningSound"))
-        {
-            StartCoroutine(WaitTuningSound());
-        }
-        else if(SceneLoader.Instance.curSceneName.Equals("SoundSource"))
-        {
-            StartCoroutine(WaitSSSound());
-        }
-
     }
 
     private void OnEnable()
@@ -40,17 +22,5 @@ public class AudioGuideManager : MonoBehaviour
     private void OnDisable()
     {
         SceneInfoAGChannel.OnAudioCue -= PlayGuideSound;
-    }
-    IEnumerator WaitHiddenSound()
-    {
-        yield return new WaitForSeconds(11f);
-    }
-    IEnumerator WaitTuningSound()
-    {
-        yield return new WaitForSeconds(8f);
-    }
-    IEnumerator WaitSSSound()
-    {
-        yield return new WaitForSeconds(8f);
     }
 }
