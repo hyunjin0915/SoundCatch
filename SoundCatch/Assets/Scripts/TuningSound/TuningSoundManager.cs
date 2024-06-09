d859c6847a0e3b8b90680e3934fc706d457ba629
su021005 <93723723+su021005@users.noreply.github.com>
2024-06-10 05:25:57 +0900

    [Fix] TuningSound 수정
    + TuningSound에서 안내 음성 재생 후 게임 실행되도록 수정
    + TuningSound에서 계속해서 다시 해 보라고 하는 음성이 재생되던 문제를 수정

--- SoundCatch/Assets/Scripts/TuningSound/TuningSoundManager.cs
+++ SoundCatch/Assets/Scripts/TuningSound/TuningSoundManager.cs
@@ -24,6 +24,9 @@ public class TuningSoundManager : MonoBehaviour
     public AudioEventChannelSO _ClickRightBlockEC;
 
     GameObject ht;
+
+    bool set = false;       // 게임 세팅용
+    float startTime = 0.0f; // 시작 타이머용
     float time = 0.0f;      // 판정 타이머용
     float asTime = 0.0f;    // 스테이지 음 반복재생용
 
@@ -38,10 +41,53 @@ public class TuningSoundManager : MonoBehaviour
         audioSource = ht.GetComponent<AudioSource>();
         subAudioSource = ht.GetComponentInChildren<AudioSource>();
         listener = GetComponent<GameObjectEventListener>();
+    }
 
+    void Update()
+    {
+        // 음성 안내 시간 동안 멈추도록
+        if(startTime <= 8.26f)
+        {
+            startTime += Time.deltaTime;
+        } else
+        {
+            if(set == false)
+            {
+                setGame();
+                set = true;
+            }
+
+            asTime += Time.deltaTime;
+            if (asTime >= 5.0f)
+            {
+                audioSource.PlayOneShot(sounds[stagePitch]);
+                asTime = 0.0f;
+            }
+
+            // 손 위치 정보 갱신
+            handPosOld = handPos;
+            handPos = ht.GetComponent<HandTracking>().getViewportPoint();
+            // UnityEngine.Debug.Log("Update. handPos : " + handPos);
+
+            // 손 위치가 변경되면 이벤트 발생
+            if (handPosOld != handPos)
+            {
+                listener.OnTSEventRaised();
+                // UnityEngine.Debug.Log("Event. handPos : " + handPos);
+            }
+
+            // 손 모양이 주먹이면 카운트
+            if (gesture == 1)
+            {
+                time += Time.deltaTime;
+            }
+        }        
+    }
+
+    void setGame()
+    {
         // 시작할 때 0~6 사이 범위 안에서 랜덤하게 스테이지 피치를 설정
         stagePitch = UnityEngine.Random.Range(0, 6);
-        UnityEngine.Debug.Log("stagePitch : " + stagePitch);
         asTime = 0.0f;
         audioSource.Stop();
         audioSource.PlayOneShot(sounds[stagePitch]);
@@ -56,37 +102,6 @@ public class TuningSoundManager : MonoBehaviour
         // 손 위치 정보 가져오기
         handPos = ht.GetComponent<HandTracking>().getViewportPoint();
         handPosOld = handPos;
-
-        UnityEngine.Debug.Log("Start. handPos : " + handPos);
-    }
-
-    void Update()
-    {
-        // 스테이지 음 반복 재생
-        asTime += Time.deltaTime;
-        if (asTime >= 5.0f)
-        {
-            audioSource.PlayOneShot(sounds[stagePitch]);
-            asTime = 0.0f;
-        }
-
-        // 손 위치 정보 갱신
-        handPosOld = handPos;
-        handPos = ht.GetComponent<HandTracking>().getViewportPoint();
-        // UnityEngine.Debug.Log("Update. handPos : " + handPos);
-
-        // 손 위치가 변경되면 이벤트 발생
-        if (handPosOld != handPos)
-        {
-            listener.OnTSEventRaised();
-            // UnityEngine.Debug.Log("Event. handPos : " + handPos);
-        }
-
-        // 손 모양이 주먹이면 카운트
-        if(gesture == 1)
-        {
-            time += Time.deltaTime;
-        }
     }
 
     public void checkAnswer()
@@ -94,8 +109,6 @@ public class TuningSoundManager : MonoBehaviour
         // viewport point의 y값에 따라 피치 변경
         int pPitchOld = pPitch;
         setpPitch();
-
-        UnityEngine.Debug.Log("pPitch : " + pPitch);
 
         if (pPitch != pPitchOld)    // 변경 시 소리 출력
         {
@@ -105,12 +118,14 @@ public class TuningSoundManager : MonoBehaviour
             pitchChange = true;
         }
 
+        // 제스처 변경 체크용
+        int oldGesture = gesture;
+
 
         // 주먹을 쥐었는지 여부 가져오기. 쥐었으면 1, 폈으면 0
         gesture = ht.GetComponent<HandTracking>().getGestureInfo();
-        UnityEngine.Debug.Log("Gesture: " + gesture);
 
-        if ((gesture == 1) && (time >= 0.75f) && (pitchChange))
+        if ((gesture == 1) && (time >= 1.0f) && (pitchChange) && (oldGesture != gesture))
         {
             // 피치를 맞췄다면
             if (pPitch == stagePitch)
@@ -136,7 +151,6 @@ public class TuningSoundManager : MonoBehaviour
                 }
                 subAudioSource.clip = sounds[8];
                 subAudioSource.Play();
-                UnityEngine.Debug.Log("Try Again");
 
             }
 
